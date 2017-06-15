8de90d117902ce1e80ea0ea0aa49bbe767c89194
Dezso Ribli <ribli@Dezsos-MacBook-Pro.local>
2017-06-15 23:36:33 +0200

    update with Dq

---
+++ dq.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Program to create a large aggregate.
+
+Created on Thu Jun 15 2017
+
+@author: ribli
+"""
+
+import random
+import scipy.optimize
+import numpy as np
+import pickle
+import datetime
+
+def get_ml(agg, rl, i=None):
+    """Get the mass list with given r list."""
+    if i is None:  # init a random pivot point
+        i = random.randint(0,len(agg)-1)
+    c = agg.keys()[i]  # get its position
+    
+    pos = np.array(agg.keys())  # get position of all points
+    dist = ((c[0]-pos[:,0])**2+(c[1]-pos[:,1])**2)**0.5  # calculate ditance
+    dist = dist[dist < rl[-1]]  # ignore what is too far
+    closest = dist < rl[0]  # select which are too close
+    m0 = np.sum(closest)  # number which are too close
+    dist = dist[~closest]  # discard too close (maybe save little time)
+    
+    r = np.sort(dist)
+    m = m0 + np.arange(len(r))
+    ml = np.interp(rl,r,m) # calculate mass for all r limits  
+    return ml
+
+
+def get_dq(agg,q,n=10, rl = xrange(20,50,5), plot=False):
+    """Calculate D(q) at a given q."""
+    # get the masses for n pivot points
+    mll=[ get_ml(agg,rl) for i in xrange(n)]  
+
+    # x,y per definition
+    x = np.log10(rl) * (q-1)
+    y = np.log10(np.mean(np.power(mll,q-1),axis=0)) 
+
+    # fit linear on log log
+    (a,b),pcov = scipy.optimize.curve_fit(lambda x,a,b: a*x+b, x, y)
+    
+    if plot:  # plot to check
+        lab = 'coef = '+ "%.2f +/- %.2f"% (a, pcov[0,0]**0.5)
+        plt.plot(x, a*x+b, label=lab)
+        plt.plot(x,y,'o')
+        plt.legend()
+    
+    return a
+
+if __name__=='__main__':
+    M = 10000
+    N = 1000
+    q = range(-9,1) + range(2,10) # q points
+    
+    agg = pickle.load(open('agg.pkl','rb')) # load large agg
+     
+    Dq=[]
+    for j in xrange(M): # multiple runs -> mean std
+        Dqi=[]
+        for qi in q:
+            Dqi.append(get_dq(agg,qi,n=N))
+            print datetime.datetime.now(),j,qi
+        Dq.append(Dqi)
+        pickle.dump((q,Dq),open('Dq.pkl','wb'))
---
+++ grow.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Program to create a large aggregate.
+
+Created on Thu Jun 15 2017
+
+@author: ribli
+"""
+
+import random
+import math
+import scipy
+import scipy.optimize
+import scipy.spatial
+import numpy as np
+import pickle
+import datetime
+
+
+def step(x,y):
+    """Random 0,1 step in random xy direction."""
+    if random.randint(0,1)==0: # x or y
+        x+=random.choice([-1,1]) # back or forth
+    else:
+        y+=random.choice([-1,1])
+    return x,y
+
+
+def walk(agg):
+    """Simlate Brown motion until attached, or out."""
+    (xc,yc),r = seed_circle(agg) # calculate seed circle
+    x,y = get_initial_pos(xc,yc,r) # start from afar
+    while True: # I like to live dangerously
+        x,y = step(x,y) # make one step
+        if too_far(x,y,xc,yc,r): # check if we got too far
+            break # if yes abandon this particle
+        # check if next to aggregate
+        if (((x-1,y) in agg) or  ((x+1,y) in agg) or
+            ((x,y-1) in agg) or  ((x,y+1) in agg) or 
+            ((x-1,y-1) in agg) or  ((x+1,y+1) in agg) or 
+            ((x-1,y+1) in agg) or  ((x+1,y-1) in agg)):
+            agg[(x,y)]=len(agg) # attach if yes
+            break # finished
+    return agg
+
+
+def seed_circle(agg):
+    """Get the seed circle."""
+    x,y = zip(*agg.keys()) # get x,y lists from dict
+    xmin,xmax,ymin,ymax = min(x),max(x),min(y),max(y) #bounds
+    xc, yc = (xmin+xmax)/2, (ymin+ymax)/2 #center
+    r = (((xmax-xmin)/2)**2 + ((ymax-ymin)/2)**2)**0.5 # radius
+    return (xc,yc),r
+
+
+def get_initial_pos(xc,yc,r):
+    """Get inital position on seed circle."""
+    ralf = 2* math.pi * random.random() # random angle
+    # initial points on the circle
+    x0 = int(xc + r*math.cos(ralf))
+    y0 = int(yc + r*math.sin(ralf))
+    return x0,y0
+
+
+def too_far(x,y,x0,y0,r,d=10):
+    """Check if the particle went too far."""
+    return ((x-x0)**2 + (y-y0)**2)**0.5 > r+d
+
+
+def grow(agg=None,npart=10000):
+    """Grow aggragate, try to attach npart points."""
+    if agg is None:  # if not contin
+        agg={(0,0):0} # 0th point at (0,0)
+    for i in range(npart):
+        agg = walk(agg) # try to attach
+    return agg
+
+
+if __name__=='__main__':
+    M = 10000
+    N = 1000
+    agg = grow(npart=N) 
+    for i in xrange(M):
+        N_oldagg = len(agg)
+        agg = grow(agg,npart=N) # create the aggregate
+        print datetime.datetime.now(),'Started',N,'particles',\
+        len(agg)-N_oldagg,'landed, agg size:',len(agg)
+        pickle.dump(agg,open('agg.pkl','wb'))
